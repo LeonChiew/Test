4a6656b0fb3fd6d86dc109ac77e90ffec128c691
LeonChiew <85923401+LeonChiew@users.noreply.github.com>
2024-06-13 19:35:54 +0800

    Update soulinvisible.js

--- script/soulinvisible.js
+++ script/soulinvisible.js
@@ -1,6 +1,29 @@
-console.log("----------------");
-let obj = JSON.parse($response.body);
+console.log("----------------------------");
+console.log("----------------------------");
 
-let body = JSON.stringify(obj);
-console.log(body);
-$done($response.body);
+
+//console.log($request.url);
+
+const url = `https://api-user.soulapp.cn/setting/visible/save?bi=%5B%2219010cad0ce%22%2C%22--%22%2C%22Apple%22%2C%22iOS%22%2C%2217.5%22%2C%2217.5%22%2C%22iPhone%2014%20Pro%22%2C%223%22%2C%22393%2A852%22%2C%22AppStore%22%2C%22WiFi%22%2C%22zh%22%5D&bik=32243&pageId=HomePage_Setup`;
+const method = `POST`;
+
+const body = `isVisible=0`;
+
+var mheaders = $request.headers;
+
+var myRequest = {
+    url: url,
+    method: method,
+    headers: mheaders,
+    body: body
+};
+
+$task.fetch(myRequest).then(response => {
+    console.log(response.statusCode + "\n\n" + response.body);
+    $done($response.body);
+}, reason => {
+    console.log(reason.error);
+    $done($response.body);
+});
+
+//$done($response.body);
